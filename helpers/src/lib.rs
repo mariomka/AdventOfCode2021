b2fd1f54f042cfb875dfa0a8991fa6cd6159c91b
Mario Juarez <mario@mjp.one>
2021-12-01 08:44:16 +0100

    Init

---
+++ helpers/src/lib.rs
@@ -0,0 +1,10 @@
+pub use input::input_lines;
+pub use input::parse_input;
+pub use input::parse_split_input;
+pub use input::split_input;
+pub use run::run;
+
+mod debug;
+mod input;
+mod instant;
+mod run;
